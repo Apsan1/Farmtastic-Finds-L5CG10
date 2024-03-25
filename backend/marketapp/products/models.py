7aa5787f9c43d72f13bf52c20f7de739788d7c47
saksham.s <sakshams982@gmail.com>
2024-03-25 20:24:39 +0545

    updated the Product model

--- backend/marketapp/products/models.py
+++ backend/marketapp/products/models.py
@@ -12,6 +12,7 @@ class Products(models.Model):
     name=models.CharField(max_length=100,blank=False)
     description=models.TextField(blank=True,null=True);
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    price=models.DecimalField(max_digits=10, decimal_places=2,default=0)
     stock=models.BooleanField(default=False)
     
 
