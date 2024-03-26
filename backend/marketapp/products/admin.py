bdb1eaec508f67129b186438ba213aa194b42f9e
saksham.s <sakshams982@gmail.com>
2024-03-26 18:28:56 +0545

    added new app for api
    this app will contain the apis for the previously created products app

--- backend/marketapp/products/admin.py
+++ backend/marketapp/products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Category)
+from .models import Category, Products
+
+
 admin.site.register(Products)
+admin.site.register(Category)
--- backend/marketapp/products/models.py
+++ backend/marketapp/products/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 
 class Category(models.Model):
-    name=models.CharField(max_length=100,blank=False)
-    
+    name = models.CharField(max_length=100, blank=False)
+
     def __str__(self):
         return self.name
-    
-    
+
+
 class Products(models.Model):
-
-    name=models.CharField(max_length=100,blank=False)
-    description=models.TextField(blank=True,null=True);
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    price=models.DecimalField(max_digits=10, decimal_places=2,default=0)
-    stock=models.BooleanField(default=False)
-    
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # ForeignKey to Category
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    stock = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-
-   
