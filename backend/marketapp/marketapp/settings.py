376f107178cab1451e9b8cd6a5820b4c98f49a18
saksham.s <sakshams982@gmail.com>
2024-04-01 21:51:09 +0545

    connected mysql db and updated requirements.txt

--- backend/marketapp/marketapp/settings.py
+++ backend/marketapp/marketapp/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = "marketapp.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MarketApp',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',  # Connect via TCP/IP
+        'PORT': '3306',
     }
+    
 }
 
 
