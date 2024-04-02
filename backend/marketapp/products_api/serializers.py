a5230cbe01ac6f8e0b69c6a906000b71985eddf7
saksham.s <sakshams982@gmail.com>
2024-04-02 13:44:03 +0545

    modified the serializer for Products

--- backend/marketapp/products_api/serializers.py
+++ backend/marketapp/products_api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from products.models import Products
+from products.models import Products,Category
 
 class ProductsSerializers(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', queryset=Category.objects.all())
     class Meta:
         model=Products
         fields='__all__'
