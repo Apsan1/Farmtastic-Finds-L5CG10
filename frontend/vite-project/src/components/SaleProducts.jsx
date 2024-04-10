4867a22ef5d8cd6e702b9bf7c1556c4671d8b5fa
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-04-10 19:27:10 +0545

    ⬆️Update: Added fetchProducts functionality

--- frontend/vite-project/src/components/SaleProducts.jsx
+++ frontend/vite-project/src/components/SaleProducts.jsx
@@ -1,56 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BsArrowRight } from 'react-icons/bs';
 import ProductCard from "./ProductCard";
+import fetchProducts  from "./fetchproducts.jsx";
 
 const SaleProducts = () => {
   const [timer, setTimer] = useState("Ends in 08:54:34");
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:8000/products/');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-    
-        // Extract image URLs
-        const imgUrls = data.map(item => item.image);
-    
-        // Fetch images and store as blobs
-        const imageBlobs = await Promise.all(imgUrls.map(fetchImage));
-    
-        // Create object URLs for each blob
-        const imageSrcs = imageBlobs.map(blob => URL.createObjectURL(blob));
-    
-        // Combine original data with imageSrcs
-        const recordsWithImages = data.map((item, index) => ({
-          ...item,
-          imageSrc: imageSrcs[index]
-        }));
-    
-        setProducts(recordsWithImages);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    
-    // Function to fetch image
-    const fetchImage = async (imageUrl) => {
-      try {
-        const response = await fetch(imageUrl);
-        if (!response.ok) {
-          throw new Error('Failed to fetch image');
-        }
-        return await response.blob();
-      } catch (error) {
-        console.error('Error fetching image:', error);
-        return null;
-      }
-    };
-    
-    fetchProducts();
+    fetchProducts().then(data => setProducts(data));
 
     // Calculate time remaining
     const endTime = new Date().getTime() + 8 * 60 * 60 * 1000 + 54 * 60 * 1000 + 34 * 1000; // Add remaining milliseconds
