5652c76ed37355d545b31db4d9caf03ce78240f5
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-04-14 07:40:46 +0545

    Fix⚒️: Image fetching issues and update UI in ProductCard and SaleProducts components

--- frontend/vite-project/src/components/homepage_navbar.jsx
+++ frontend/vite-project/src/components/homepage_navbar.jsx
@@ -7,12 +7,16 @@ function handleCartClick() {
   console.log("Cart clicked");
   window.location.href = "/cart";
 }
+function handleLogoClick(){
+  console.log("Logo clicked");
+  window.location.href = "/";
+}
 const homepage_navbar = () => {
   return (
     <div className="sticky top-0 bg-white z-10">
       <div className="container lg:block">
         <div className="flex justify-between items-center p-8">
-          <h1 className="text-4xl font-medium">Farmtastic Finds</h1>
+          <h1 className="text-4xl font-medium" onClick={handleLogoClick}>Farmtastic Finds</h1>
           <div className="relative w-full max-w-[500px]">
             <input
               className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full"
