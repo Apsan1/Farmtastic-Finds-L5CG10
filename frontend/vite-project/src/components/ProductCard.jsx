34d6e09194c957c387bcb31606497d6636387cab
sakshamS982 <159793966+saksham310@users.noreply.github.com>
2024-04-15 22:17:42 +0545

    Revert "Added add quantity feature"

--- frontend/vite-project/src/components/ProductCard.jsx
+++ frontend/vite-project/src/components/ProductCard.jsx
@@ -2,28 +2,21 @@ import React, { useContext } from "react";
 import { AiFillStar, AiOutlineStar, AiOutlineShopping, AiFillHeart } from "react-icons/ai";
 import { CartContext } from "../context/cart";
 
+function ProductCard(props) {
+  const { id, img, name, price, quantity = 1 } = props;
 
-function ProductCard (props) {
-  const {id, img, name, price} = props;
-  const {cartItems, setCartItems} = useContext(CartContext);
-  const [quantity,setQuantity]=useState(1);
 
+  const handleClickProducts = (id) => {
+    window.location.href = `/product/${id}`;
+  };
+  const { cartItems, setCartItems } = useContext(CartContext);
   const handleClick = () => {
-   
-    setCartItems(prev =>[...prev,{...props,totalQuantity:quantity}]);
-       
-  }
-
-  //function to update quantity of the products
-  const handleQuantity=(value)=>{
-    const newQuantity = Math.max(0, quantity + value);
-    setQuantity(newQuantity);
-  }
+    setCartItems(prev => [...prev, props]);
+  };
 
   return (
-    <div className="border border-gray-300 border-2 hover:border-gray-300 hover:scale-105 transition-transform rounded-lg-relative">
-      <img src={img} alt={name} style={{ height: "10em" }} />
-
+    <div className="border border-gray-300 border-2 hover:border-gray-300 hover:scale-105 transition-transform rounded-lg relative" onClick={() => handleClickProducts(id)}>
+      <img src={img} alt={name} />
       <div className="space-y-2 relative p-4">
         <div className="text-yellow-400 flex gap-[2px] text-[20px]">
           <AiFillStar />
@@ -34,21 +27,6 @@ function ProductCard (props) {
         </div>
         <h3 className="font-medium">{name}</h3>
         <h3 className="text-2xl font-medium text-orange-600">{price}</h3>
-        <div className="flex items-center absolute -bottom-0 right-1">
-          <button
-            className="bg-accent text-white p-1 rounded-full grid place-items-center cursor-pointer"
-            onClick={()=>handleQuantity(-1)}
-          >
-            -
-          </button>
-          <span className="px-2">{quantity}</span>
-          <button
-            className="bg-accent text-white p-1 rounded-full grid place-items-center cursor-pointer"
-            onClick={()=>handleQuantity(1)}
-          >
-            +
-          </button>
-        </div>
         <button
           className="absolute -top-4 right-1 bg-accent text-white text-[28px] w-[45px] h-[45px] rounded-full grid place-items-center cursor-pointer"
           onClick={handleClick}
