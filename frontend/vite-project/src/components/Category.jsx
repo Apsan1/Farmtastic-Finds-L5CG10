7b3fe9479ac45503f65077b7c24d5939a0f021e6
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-04-23 07:18:31 +0545

    Bug🪲: fixes deletion of src folder

---
+++ frontend/vite-project/src/components/Category.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import CategoryCard from "./CategoryCard";
+import { fetchCategories } from "./fetchproducts";
+
+const Category = () => {
+  const [categoryData, setCategoryData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchCategories();
+        setCategoryData(data);
+      } catch (error) {
+        console.error('Error fetching category data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array to run effect only once on mount
+
+  return (
+    <div className="container">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {categoryData.map((el, index) => (
+          <CategoryCard 
+            key={index}
+            img={el.cat_image} // Assuming you have 'img' property in your category data
+            name={el.name} // Assuming 'category' property contains category name
+            count={el.count}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Category;
