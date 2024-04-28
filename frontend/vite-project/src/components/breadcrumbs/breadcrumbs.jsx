f505d04a36654f8aae9e829f804fa3c79b55e066
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-04-28 21:14:45 +0545

    Feat✨: Refactor breadcrumbs component to use dynamic nature

--- frontend/vite-project/src/components/breadcrumbs/breadcrumbs.jsx
+++ frontend/vite-project/src/components/breadcrumbs/breadcrumbs.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import './breadcrumbs.css'
 import { BiArrowFromLeft } from 'react-icons/bi';
 
-/*
-Uncomment this code and use it in the codes below if you want to use the onclick functions.
 
 function onclickCatalog (category) {
   window.location.href = `/category/${category}`; //redirect to category page
@@ -17,36 +15,34 @@ function onclickHome () {
   window.location.href = `/`; //redirect to home page
 }
 
-*/
+function onclickShop () {
+  window.location.href = `/shop`; //redirect to shop page
+}
 
 const Breadcrum = (props) => {
     const {product} =props;
   return (
-    <div className='breadcrum flex flex-row gap-2 justify-left items-center'>
-      HOME<img src={BiArrowFromLeft} alt="" />SHOP <img src={BiArrowFromLeft} alt=""/> {product.category} <img src={BiArrowFromLeft} alt=""/> {product.name}
+    <>
+    <div className="flex flex-row items-center gap-2">
+    <button onClick={()=>onclickHome()}>Home</button><BiArrowFromLeft/> 
+    <button onClick={()=>onclickShop()}>Shop</button> <BiArrowFromLeft/>
+    <button onClick={()=>onclickCatalog(product.category)}>{product.category}</button><BiArrowFromLeft/>
+    <button onClick={()=>onclickProduct(product)}>{product.name}</button> 
     </div>
+    </>
   )
 }
-
-{/* 
-
-Just an Idea you can use on both above and below code
-
-<button onClick={()=>onclickHome()}>Home</button><BiArrowFromLeft/> 
-<button onClick={()=>onclickShop()}>Shop</button> <BiArrowFromLeft/>
-<button onClick={()=>onclickCatalog(product.category)}>{product.category}</button> 
-<BiArrowFromLeft/>
-<button onClick={()=>onclickProduct(product)}>{product.name}</button> 
-
-*/}
 
 export const Categorybreadcrum = (props) => {
   const {category} =props;
   const name = category[0];
   return (
-    <div className='breadcrum flex flex-col'>
-      HOME<img src={BiArrowFromLeft} alt="" />SHOP <img src={BiArrowFromLeft} alt=""/> {name}
-    </div>
+    <>
+    <div className="flex flex-row items-center gap-2">
+    <button onClick={()=>onclickHome()}>Home</button><BiArrowFromLeft/>
+    <button onClick={()=>onclickShop()}>Shop</button> <BiArrowFromLeft/>
+    <button onClick={()=>onclickCatalog(name)}>{name}</button></div>
+    </>
   )
 }
 
