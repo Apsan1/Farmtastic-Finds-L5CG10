949bd7c185133a0cd1ddabf035774d816afd607c
saksham.s <sakshams982@gmail.com>
2024-05-02 16:05:09 +0545

    Added login functionality to the backend/frontend

--- frontend/vite-project/src/cart.jsx
+++ frontend/vite-project/src/cart.jsx
@@ -24,28 +24,28 @@ const Cart = () => {
             <div className="space-x-4 mt:8 lg:mt-0">
                     <button className="trending_btn" onClick={handleShopClick} >Back</button>
                 </div>
-                <div class="flex items-center justify-center">
-      <h1 class="text-2xl font-semibold text-gray-900">My Cart</h1>
+                <div className="flex items-center justify-center">
+      <h1 className="text-2xl font-semibold text-gray-900">My Cart</h1>
     </div>
-          <div class="mx-auto mt-8 max-w-2xl md:mt-12">
-      <div class="bg-white shadow">
-        <div class="px-4 py-6 sm:px-8 sm:py-10">
-          <div class="flow-root">
-            <ul class="-my-8">
-              <li class="flex flex-col space-y-4 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
+          <div className="mx-auto mt-8 max-w-2xl md:mt-12">
+      <div className="bg-white shadow">
+        <div className="px-4 py-6 sm:px-8 sm:py-10">
+          <div className="flow-root">
+            <ul className="-my-8">
+              <li className="flex flex-col space-y-4 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
                 <CartCard/></li>  </ul>
           </div>
         </div>
         </div>
-        <div class="mt-6 flex items-center justify-between">
-            <p class="text-sm font-medium text-gray-900">Total</p>
-            <p class="text-2xl font-semibold text-gray-900"><span class="text-s font-bold text-gray-400">$</span>{total}</p>
+        <div className="mt-6 flex items-center justify-between">
+            <p className="text-sm font-medium text-gray-900">Total</p>
+            <p className="text-2xl font-semibold text-gray-900"><span className="text-s font-bold text-gray-400">$</span>{total}</p>
           </div>
-          <div class="mt-6 text-center">
-            <button type="button" class="group inline-flex w-full items-center justify-center rounded-md trending_btn px-6 py-4 text-lg font-semibold text-white transition-all duration-200 ease-in-out focus:shadow hover:trending__btn">
+          <div className="mt-6 text-center">
+            <button type="button" className="group inline-flex w-full items-center justify-center rounded-md trending_btn px-6 py-4 text-lg font-semibold text-white transition-all duration-200 ease-in-out focus:shadow hover:trending__btn">
               Checkout
-              <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-6 w-6 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+              <svg xmlns="http://www.w3.org/2000/svg" className="group-hover:ml-8 ml-4 h-6 w-6 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
               </svg>
             </button>
           </div>
