e43e30a4d3dd89bcad4cb6154bea8becff178750
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-05-04 12:50:12 +0545

    Fix⚒️: Category and CategoryCard components styling

--- frontend/vite-project/src/components/CategoryCard.jsx
+++ frontend/vite-project/src/components/CategoryCard.jsx
@@ -4,15 +4,15 @@ const onCategoryClick = (category) => {
 
 const CategoryCard = ({ img, name, count }) => {
     return (
-      <div className="mt-4 border border-gray-300 border-2 hover:border-gray-300 hover:scale-105 transition-transform rounded-lg " onClick={()=>onCategoryClick(name)}>
+      <div className="mt-4 border border-gray-300 shadow-3xl hover:border-gray-300 hover:scale-105 transition-transform rounded-lg " onClick={()=>onCategoryClick(name)}>
         <div className="flex flex-col justify-between">
         <div className="flex justify-between items-center p-6">
           <h3 className="font-medium text-xl">{name}</h3>
           <p className="text-gray-500">{count} {count === 1 ? 'Product' : 'Products'}</p>
         </div>
 
-        <div className="flex justify-center items-center">
-        <img className="w-[250px] h-auto m-2 rounded-xl" src={img} alt={name} />
+        <div className="flex w-100 h-50 justify-center items-center p-5">
+          <img className="object-contain max-w-full max-h-full rounded-lg" src={img} alt={name} />
         </div>
         </div>
       </div>
