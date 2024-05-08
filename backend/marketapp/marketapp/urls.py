70c29a774cc99f7f6aef5e83836c4db54960c7bd
saksham.s <sakshams982@gmail.com>
2024-05-08 13:25:13 +0545

    added missing url path to the backend

--- backend/marketapp/marketapp/urls.py
+++ backend/marketapp/marketapp/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("products_api.urls")),
     path('payment/',include("payment.urls")),
+    path('dashboard/',include("login.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
