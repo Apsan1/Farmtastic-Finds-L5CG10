54c525a37231009ba779be866caf5364149c35b4
Apsan <np03cs4a220109@heraldcollege.edu.np>
2024-05-08 15:10:12 +0545

    Refactor: Improve responsiveness and styling in CategoryCard component

--- frontend/vite-project/src/components/CategoryCard.jsx
+++ frontend/vite-project/src/components/CategoryCard.jsx
@@ -6,9 +6,9 @@ const CategoryCard = ({ img, name, count }) => {
     return (
       <div className="mt-4 border border-gray-300 shadow-3xl hover:border-gray-300 hover:scale-105 transition-transform rounded-lg " onClick={()=>onCategoryClick(name)}>
         <div className="flex flex-col justify-between">
-        <div className="flex justify-between items-center p-6">
+        <div className="flex justify-between items-center px-4 py-6 ">
           <h3 className="font-medium text-xl">{name}</h3>
-          <p className="text-gray-500">{count} {count === 1 ? 'Product' : 'Products'}</p>
+          <p className="text-gray-500 text-[0.9rem] text-center">{count} {count === 1 ? 'Product' : 'Products'}</p>
         </div>
 
         <div className="flex w-100 h-50 justify-center items-center p-5">
