11ddf759c20daa74fe8f38fc8e81e712277d217e
saksham.s <sakshams982@gmail.com>
2024-05-08 07:23:05 +0545

    Changed the color theme for admin login

--- frontend/vite-project/src/components/admin.jsx
+++ frontend/vite-project/src/components/admin.jsx
@@ -24,7 +24,7 @@ const Admin = () => {
             const data = await response.json();
             console.log(data.message);
             if(data.message==="Logged in successfully"){
-                window.location.href='/cart';
+                window.location.href='/dashboard';
             }
         } catch (error) {
             console.error('Error:', error.message);
@@ -38,26 +38,26 @@ const Admin = () => {
                 <div className="relative flex flex-col sm:w-[30rem] rounded-lg border-gray-400 bg-white shadow-lg px-4">
                     <div className="flex-auto p-6">
                         <div className="mb-10 flex flex-shrink-0 flex-grow-0 items-center justify-center overflow-hidden">
-                            <a href="#" className="flex cursor-pointer items-center gap-2 text-black-900 no-underline hover:text-black-500">
-                                <span className="flex-shrink-0 text-3xl font-black  tracking-tight opacity-100">Farmtastic Finds</span>
+                            <a href="#" className="flex cursor-pointer items-center gap-2 text-green-600 no-underline hover:text-green-500">
+                                <span className="flex-shrink-0 text-3xl font-green  tracking-tight opacity-100">Farmtastic Finds</span>
                             </a>
                         </div>
-                        <h4 className="mb-2 font-medium text-gray-700 xl:text-xl">Welcome to Admin Dashboard!</h4>
+                        <h4 className="mb-2 font-medium text-green-700 xl:text-xl">Welcome to Admin Dashboard!</h4>
                         <form className="mb-4" onSubmit={handleLogin}>
                             <div className="mb-4">
-                                <label htmlFor="username" className="mb-2 inline-block text-xs font-medium uppercase text-gray-700">Username</label>
+                                <label htmlFor="username" className="mb-2 inline-block text-xs font-medium uppercase text-green-700">Username</label>
                                 <input type="text" onChange={(e)=>setUsername(e.target.value)} className="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg-gray-100 py-2 px-3 text-sm outline-none focus:border-black focus:bg-white focus:text-gray-600 focus:shadow" id="username" name="username" placeholder="Enter your username" autoFocus />
                             </div>
                             <div className="mb-4">
             
                             <div className="mb-4">
-                                <label htmlFor="password" className="mb-2 inline-block text-xs font-medium uppercase text-gray-700">Password</label>
+                                <label htmlFor="password" className="mb-2 inline-block text-xs font-medium uppercase text-green-700">Password</label>
                                 <input type="password" onChange={(e)=>setPassword(e.target.value)} className="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg-gray-100 py-2 px-3 text-sm outline-none focus:border-black focus:bg-white focus:text-gray-600 focus:shadow" id="password" name="password" placeholder="*****" autoFocus />
                             </div>
                             </div>
 
                             <div className="mb-4">
-                            <button className="grid w-full cursor-pointer select-none rounded-md border border-gray-500 bg-black py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-black-600 hover:bg-black hover:text-white focus:border-indigo-600 focus:bg-indigo-600 focus:text-white focus:shadow-none" type="submit">Log In</button>
+                            <button className="grid w-full cursor-pointer select-none rounded-md border border-gray-500 bg-green-700 py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-black-600 hover:bg-green-600 hover:text-white focus:border-indigo-600 focus:bg-indigo-600 focus:text-white focus:shadow-none" type="submit">Log In</button>
 
                             </div>
                         </form>
