c62181726c12ba82028b45ee9e67fc2d7b650443
Neema <np03cs4a220322@heraldcollege.edu.np>
2024-05-10 23:01:34 +0545

    Updated main for dashboard.⬆️

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
-import Hero from './components/Hero.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Hero from "./components/Hero.jsx";
 import Homepage_navbar from "./components/homepage_navbar";
 import Mob_homepage_nav from "./components/Mob_homepage_nav.jsx";
-import { CartProviders } from './context/cart.jsx';
-import Shoppage  from './shoppage.jsx';
-import VideoBackground from './components/video-background.jsx';
-import Footer from './components/footer.jsx';
-import Cart from './cart.jsx';
-import ProductDetails from './ProductDetails.jsx';
-import { SaleProductsFilteredByCategory } from './components/SaleProducts.jsx';
-import Admin from './components/admin.jsx';
-import Checkout from './components/checkout.jsx';
-import { SaleProductsFilteredBySearch } from './components/SaleProducts.jsx';
-import Error from './components/outputPage.jsx';
+import { CartProviders } from "./context/cart.jsx";
+import Shoppage from "./shoppage.jsx";
+import VideoBackground from "./components/video-background.jsx";
+import Footer from "./components/footer.jsx";
+import Cart from "./cart.jsx";
+import ProductDetails from "./ProductDetails.jsx";
+import { SaleProductsFilteredByCategory } from "./components/SaleProducts.jsx";
+import Admin from "./components/admin.jsx";
+import Checkout from "./components/checkout.jsx";
+import { SaleProductsFilteredBySearch } from "./components/SaleProducts.jsx";
+import Error from "./components/outputPage.jsx";
+import Dashboard from "./admin_dashboard.jsx";
+import Orders from "./components/Order.jsx";
+import AddProduct from "./components/AddProductForm.jsx";
+import ProductList from "./components/Product_list.jsx";
+import ProfileSettings from "./components/ProfileSettings.jsx";
 
 const RouterPaths = () => {
   return (
@@ -26,14 +31,22 @@ const RouterPaths = () => {
         <Route path="/cart" element={<CartComponent />} />
         <Route path="/admin/login" element={<AdminComponent />} />
         <Route path="/product/:id" element={<ProductDetailsComponent />} />
-        <Route path="/category/:category" element={<SaleProductsFilteredByCategoryComponent />} />
+        <Route
+          path="/category/:category"
+          element={<SaleProductsFilteredByCategoryComponent />}
+        />
         <Route path="/search/:search" element={<SearchPageComponent />} />
         <Route path="/checkout" element={<CheckoutComponent />} />
+        <Route path="/dashboard" element={<DashboardComponent />} />
+        <Route path="/order" element={<OrderComponent />} />
+        <Route path="/add_product" element={<AddProductComponent />} />
+        <Route path="/product_list" element={<ProductListComponent />} />
+        <Route path="/profile" element={<ProfileComponent />} />
         <Route path="/*" element={<OutputPage />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
 const MainComponent = () => {
   return (
@@ -61,7 +74,7 @@ const CartComponent = () => {
   return (
     <React.StrictMode>
       <CartProviders>
-      <Homepage_navbar />
+        <Homepage_navbar />
         <Cart />
       </CartProviders>
     </React.StrictMode>
@@ -77,7 +90,7 @@ const ProductDetailsComponent = () => {
       </CartProviders>
     </React.StrictMode>
   );
-}
+};
 
 const SaleProductsFilteredByCategoryComponent = () => {
   return (
@@ -88,22 +101,22 @@ const SaleProductsFilteredByCategoryComponent = () => {
       </CartProviders>
     </React.StrictMode>
   );
-}
-const AdminComponent=()=>{
-  return(
+};
+const AdminComponent = () => {
+  return (
     <React.StrictMode>
       <Admin></Admin>
     </React.StrictMode>
-  )
-}
+  );
+};
 
-const CheckoutComponent=()=>{
-  return(
+const CheckoutComponent = () => {
+  return (
     <React.StrictMode>
       <Checkout></Checkout>
     </React.StrictMode>
-  )
-}
+  );
+};
 
 const SearchPageComponent = () => {
   return (
@@ -125,6 +138,42 @@ const OutputPage = () => {
       </CartProviders>
     </React.StrictMode>
   );
-}
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterPaths />);
+const DashboardComponent = () => {
+  return (
+    <React.StrictMode>
+      <Dashboard />
+    </React.StrictMode>
+  );
+};
+const OrderComponent = () => {
+  return (
+    <React.StrictMode>
+      <Orders />
+    </React.StrictMode>
+  );
+};
+const AddProductComponent = () => {
+  return (
+    <React.StrictMode>
+      <AddProduct />
+    </React.StrictMode>
+  );
+};
+const ProductListComponent = () => {
+  return (
+    <React.StrictMode>
+      <ProductList />
+    </React.StrictMode>
+  );
+};
+const ProfileComponent = () => {
+  return (
+    <React.StrictMode>
+      <ProfileSettings />
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<RouterPaths />);
