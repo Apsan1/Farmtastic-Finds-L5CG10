e8e027c7e8dfbee479de2bd1ea87a189f4660662
saksham.s <sakshams982@gmail.com>
2024-05-12 18:14:37 +0545

    added icons for product and category , also changed index title

--- backend/marketapp/products/admin.py
+++ backend/marketapp/products/admin.py
@@ -1,7 +1,21 @@
-from django.contrib import admin
-from .models import Category, Products
+# from django.contrib import admin
+# from .models import Category, Products
 
 
-admin.site.register(Products)
-admin.site.register(Category)
+# admin.site.register(Products)
+# admin.site.register(Category)
 
+from django.contrib.admin import ModelAdmin, register
+
+
+from products.models import Products,Category
+
+@register(Products)
+class ProductAdmin(ModelAdmin):
+    list_display = ('name','description','category','stock','price','image')
+    icon_name='donut_small'
+
+@register(Category)
+class CategoryAdmin(ModelAdmin):
+    list_display = ('name','cat_image')
+    icon_name='donut_small'
--- backend/marketapp/products/apps.py
+++ backend/marketapp/products/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ProductsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "products"
+    icon_name='donut_small'
